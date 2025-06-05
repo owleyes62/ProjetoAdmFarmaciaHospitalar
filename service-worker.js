dbe7b19547c83c107878cec3f0962bf453120f0d
Iwerson <109252776+owleyes62@users.noreply.github.com>
2025-06-05 23:47:49 +0000

    preparando pwa connect

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'adm-hospitalar-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/css/style.css',
+  // adicione outras rotas e arquivos importantes
+];
+
+// Instala e armazena os arquivos em cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Ativa e limpa caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Intercepta as requisições e tenta servir do cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
